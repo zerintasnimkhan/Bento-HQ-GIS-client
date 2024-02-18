3560badca6250b3b02868d0595fa95582acf6d8a
Zerin Tasnim <zerintasnimriana@gmail.com>
2024-02-18 15:01:33 +0600

    feat: style changes

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,32 +1,44 @@
 import React from "react";
-import styles from "../styles/login.module.css"; // Import your styles file
+import Lottie from "react-lottie";
+import styles from "../styles/login.module.css";
+import animationData from "../../public/lottie/animationData.json";
 
 const LoginPage = () => {
+  const lottieOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
     <div className={styles.container}>
       {/* Left side with image */}
-      <div className={styles.imageContainer}>
-        <img
-          className={styles.image}
-          src="your-image-url.jpg"
-          alt="Login Page Image"
-        />
+      <div className={styles.image}>
+        <Lottie options={lottieOptions} height={760} width={850} />
       </div>
+
 
       {/* Right side with login form */}
       <div className={styles.formContainer}>
         <h2>Login</h2>
         <form className={styles.loginForm}>
           {/* Add your form fields here (e.g., username, password) */}
-          <label>
+          <label className={styles.label}>
             Username:
-            <input type="text" name="username" />
+            <input className={styles.input} type="text" name="username" />
           </label>
-          <label>
+          <label className={styles.label}>
+            Email:
+            <input className={styles.input} type="text" name="username" />
+          </label>
+          <label className={styles.label}>
             Password:
-            <input type="password" name="password" />
+            <input className={styles.input}type="password" name="password" />
           </label>
-          <button type="submit">Login</button>
+          <button className={styles.button} type="submit">Login</button>
+          <button className={styles.button} type="submit">sign up</button>
         </form>
       </div>
     </div>
