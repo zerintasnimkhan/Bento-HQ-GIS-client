72fd4735b8393476e524f6e3ea53b8d94bd1fefa
Zerin Tasnim <zerintasnimriana@gmail.com>
2024-02-18 19:10:24 +0600

    feat: useRouter implementation instead of useNavigate

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { login } from "../services/auth.service";
+// import { useNavigate } from "react-router-dom";
+import { useRouter } from "next/router";
 import Lottie from "react-lottie";
 import styles from "../styles/login.module.css";
 import animationData from "../../public/lottie/animationData.json";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // const navigate = useNavigate();
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    try {
+      const res = await login(email, password);
+      const token = res;
+      localStorage.setItem("access-token", token);
+      router.push('/map');
+      // navigate("/map");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const lottieOptions = {
     loop: true,
     autoplay: true,
     animationData: animationData,
     rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice",
+      preserveAspectRatio: "xMidYMid slice",
     },
   };
   return (
@@ -18,7 +39,6 @@ const LoginPage = () => {
       <div className={styles.image}>
         <Lottie options={lottieOptions} height={760} width={850} />
       </div>
-
 
       {/* Right side with login form */}
       <div className={styles.formContainer}>
@@ -31,14 +51,30 @@ const LoginPage = () => {
           </label>
           <label className={styles.label}>
             Email:
-            <input className={styles.input} type="text" name="username" />
+            <input
+              className={styles.input}
+              type="text"
+              name="username"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </label>
           <label className={styles.label}>
             Password:
-            <input className={styles.input}type="password" name="password" />
+            <input
+              className={styles.input}
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </label>
-          <button className={styles.button} type="submit">Login</button>
-          <button className={styles.button} type="submit">sign up</button>
+          <button className={styles.button} type="submit" onClick={handleLogin}>
+            Login
+          </button>
+          <button className={styles.button} type="submit">
+            sign up
+          </button>
         </form>
       </div>
     </div>
---
+++ src/services/auth.service.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+const baseUrl = 'http://localhost:3001';
+
+export async function login (email, password) {
+  try {
+    const url = `${baseUrl}/login`;
+    const res = await axios.post(url, { email, password });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw new Error('Error logging in.');
+  }
+}
+
+export async function signup (name, email, password) {
+  try {
+    const url = `${baseUrl}/register`;
+    console.log(name, email, password)
+    const res = await axios.post(url, { name, email, password });
+    console.log(res)
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw new Error('Error signing up.');
+  }
+}
+
+
+export async function getUserFromToken (token) {
+  try {
+    const url = `${baseUrl}/auth/user`;
+    const res = await axios.get(url, { headers: {Authorization: `Bearer ${token}`}});
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw new Error('Error signing up.');
+  }
+}
