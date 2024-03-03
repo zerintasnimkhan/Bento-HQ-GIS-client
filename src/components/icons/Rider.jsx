b0b83da91817137846f9e1550c7fc2deb7e4ea56
Zerin Tasnim <zerintasnimriana@gmail.com>
2024-03-03 20:00:44 +0600

    feat: changed rider icon size a bit

--- src/components/icons/Rider.jsx
+++ src/components/icons/Rider.jsx
@@ -4,7 +4,7 @@ function RiderIcon() {
   return (
  
     <img
-      height={28}
+      height={30}
       width={28}
       src="https://res.cloudinary.com/dwrwwcvfb/image/upload/v1708583837/delivery_ltpejd.png"
       alt=""
